61d693913d49b6f92858610778826dadac3da44b
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2023-11-29 21:03:46 -0500

    Add scripting

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const goToUrl = (url) => window.open(url, "_blank")
+const hireButton = document.querySelector("#hire-button")
+
+// Prevent default behavior on all # links
+document.querySelectorAll('a[href="#"]').forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault()
+    console.log("Link clicked:", link.id)
+  })
+})
+
+function changeBodyBackground() {
+  document.body.style.backgroundColor = "rebeccapurple"
+}
+
+function restorBodyBackground() {
+  document.body.style.backgroundColor = "white"
+}
+
+hireButton.addEventListener("mouseenter", changeBodyBackground)
+hireButton.addEventListener("mouseleave", restorBodyBackground)
